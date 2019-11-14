17f96aedd60eb1d0519760b0c73c94cc305da712
Kiwilynn <kasll@hotmail.com>
2019-11-14 11:11:39 +0100

    index side, css og scripts tilføjet, samt billeder

---
+++ test/scripts.js
@@ -0,0 +1,34 @@
+var x = document.getElementsByClassName("IntroPagePicture");
+var y = 0;
+
+
+function slider() {
+
+    window.setInterval(Intropager, 11000);
+}
+
+function Intropager() {
+    
+        console.log(y)
+
+    x[y].style.transform = "translateX(-2000px)";
+
+    y++
+
+        if (y > (x.length)-1) {
+            y = 0;
+            for (i = 0; i <= x.length - 1; i++) {
+                x[i].style.transform = "translateX(0px)";
+                
+            }
+        };
+
+    }
+    
+
+function NavBar() {
+
+    if (document.documentElement.scrollTop > 25) {
+        document.getElementById("NavBar").style.marginTop = "0px";
+    } 
+}
