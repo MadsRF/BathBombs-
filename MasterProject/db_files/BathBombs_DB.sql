1f54759d2b65cfe81c6311deebe1863e190b27c0
Oliver Kramer <3520_kramer@live.dk>
2019-12-04 12:08:38 +0100

    Changed folder structure of project. Final integration so far

---
+++ MasterProject/db_files/BathBombs_DB.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE IF NOT exists BathBombs_DB;
+USE BathBombs_DB;
+
+CREATE TABLE customers(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+firstname varchar(30) NOT NULL,
+lastname varchar(30) NOT NULL,
+address varchar(30) NOT NULL,
+postal_code varchar(30) NOT NULL,
+city varchar(30) NOT NULL,
+country varchar(30) NOT NULL,
+email varchar(50) NOT NULL, 
+tlf varchar(20)
+);
+
+INSERT INTO customers VALUES(default, "mads", "hansen", "håndvej", "2450", "KBH", "Denmark","mads@mads.dk", "12345678");
+
+CREATE TABLE users(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+firstname varchar(30) NOT NULL,
+lastname varchar(30),
+address varchar(30),
+postal varchar(30),
+city varchar(30),
+country varchar(30), 
+email varchar(50) NOT NULL, 
+tlf varchar(20),
+username varchar(30) NOT NULL UNIQUE,
+password varchar(30) NOT NULL,
+fk_customer_id int NOT NULL, 
+
+FOREIGN KEY(fk_customer_id)references customers(id)
+);
+
+INSERT INTO users VALUES(default, "kasper", "kaspersen", "håndvej", "2500", "KBH SV", "Denmark","KASPER@mads.dk", "99999999", "Teletubi", "1234", "1");
+
+
+CREATE TABLE products(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+name varchar(50) NOT NULL,
+price decimal(10,2) NOT NULL,  
+description varchar(250) NOT NULL,
+category varchar(50) NOT NULL
+); 
+
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "100", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "800", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "600", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "400", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "200", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "250", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "350", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "450", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "666", "this is a bathbomb!", "bathbomb");
+INSERT INTO products VALUES(DEFAULT, "BATHBOMB", "6969", "this is a bathbomb!", "bathbomb");
+
+CREATE TABLE owner(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+username varchar(30) NOT NULL UNIQUE,
+password varchar(30) NOT NULL
+);
+
+INSERT INTO owner VALUES(DEFAULT, "GERT", "12345");
+
+
+
+
---
+++ MasterProject/website_files/js/scripts.js
@@ -0,0 +1,75 @@
+var x = document.getElementsByClassName("IntroPagePicture");
+var y = 0;
+
+
+function slider() {
+
+    window.setInterval(Intropager, 11000);
+}
+
+// Gets all information about products from the API-products and display them on frontpage
+$.get("http://localhost:9000/api-products", function(data){
+    console.log('hej');
+    var html = '';
+
+    for(i = 0; i < data.length; i++){
+        html += '<div class="item"> ' +
+            '<img class="pic" src="../pictures/bath7.jpg" title="Bathbomb for now" alt="Bathbomb for now"> ' +
+            '<p class="productTitle" >' + data[i].name + '</p>' +
+            '<p class="descriptionText">' + data[i].description + '</p>' +
+            '<p class="priceText">' + data[i].price + ' kr' + '</p></div>'
+    }
+    $('.frontPageProductWrapper').append(html);
+
+});
+
+function Intropager() {
+    
+        console.log(y)
+
+    x[y].style.transform = "translateX(-2000px)";
+
+    y++
+
+        if (y > (x.length)-1) {
+            y = 0;
+            for (i = 0; i <= x.length - 1; i++) {
+                x[i].style.transform = "translateX(0px)";
+                
+            }
+        };
+
+    }
+    
+
+function NavBar() {
+
+    if (document.documentElement.scrollTop > 25) {
+        document.getElementById("NavBar").style.marginTop = "0px";
+    } 
+}
+
+function direct(){
+    window.location.href="URL";
+}
+function directCheckout(){
+    window.location.href="URL";
+
+}
+
+var basketboolean = true;
+function viewBasket() {
+
+
+    if (basketboolean) {
+        document.getElementById("basket-container").style.visibility = "visible";
+        basketboolean=false;
+    }else if (!basketboolean){
+        document.getElementById("basket-container").style.visibility = "hidden";
+        basketboolean=true;
+
+
+
+
+    }
+}
